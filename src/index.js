edd3c9dd9063a40821db777ad1d04e134bc08610
Ashish <himanashish446@gmail.com>
2023-11-12 01:33:29 +0530

    pizza app completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// imoprt css file
+import './index.css'
+
 
 // an example of the JSX use
 // function formatName(user) {
@@ -64,21 +67,99 @@ const pizzaData = [
 
 function App() {
     return (
-        <div>
-            <h1>Greetings Ashish!! </h1>
-            <Pizza />
-            <Pizza />
-            <Pizza />
+        <div className="container">
+            <Header />
+            <Menu />
+            <Footer />
         </div>
     )
 
 }
-function Pizza() {
-    return <div>
-        <img src="pizzas/spinaci.jpg" alt="spinaci.jpg"/>
-        <h1>Pizza Prosciutto</h1>
-        <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
-    </div>
+
+// header function
+function Header() {
+    return (
+        <header className="header">
+            <h1 >Pizza Factory </h1>
+        </header>
+    )
+}
+
+// menu function
+function Menu() {
+    const pizzas = pizzaData;
+    // const pizzas = [];
+
+    const numPizzas = pizzas.length
+    return (
+        <main className="menu">
+            <h2>Our Menu</h2>
+            
+            {numPizzas > 0 ? (
+                <>
+                    <p>Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various ...</p>
+                <ul className="pizzas">
+                    {pizzas.map(pizza => <Pizza pizzaObj={pizza} key={pizza.name} />)}
+                </ul>
+                </>
+            ) : <p>Still working on menu, please come back later</p>}
+
+
+
+            {/* <Pizza name='Pizza Spinaci' ingredients='Tomato, mozarella, ham, aragula, and burrata cheese' photoName='pizzas/spinaci.jpg' price={10} />
+            <Pizza name='Pizza fungi' ingredients='Tomato, mushroom, ham, aragula, and burrata cheese' photoName='pizzas/funghi.jpg' price={15} /> */}
+        </main>
+    )
+}
+
+// props passing data from cosnt data to Pizza component
+
+function Pizza({pizzaObj}) {
+    // check whats in props by console
+    console.log(pizzaObj);
+    // if pizza is sold out we can return null using line below
+    // if (pizzaObj.soldOut) return null;
+    return (
+        <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
+            <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+            <div>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredients}</p>
+                {/* example of ternary operator on span below */}
+                <span>{pizzaObj.soldOut ? "Sold Out" : pizzaObj.price}</span>
+            </div>
+        </li>
+    )
+}
+
+// footer function
+function Footer() {
+    const hour = new Date().getHours();
+    console.log(hour);
+    const openHour = 10;
+    const closeHour = 22;
+    // if(hour>=openHour && hour<=closeHour){
+    //     alert("We're Open")
+    // }else{
+    //     alert("We're Close")
+    // }
+    const isOpen = hour >= openHour && hour <= closeHour;
+    console.log(isOpen);
+
+    return (
+        <footer className="footer">{isOpen ? (
+            <Order closeHour={closeHour} openHour={openHour}/>
+        ) : <p>Currently closed, Happly to welcome between {openHour}:00 and {closeHour}:00 pm</p>}</footer>
+    )
+}
+
+function Order({closeHour, openHour}) {
+    return (
+        <div className="order">
+            <p>We're Open from {openHour}:am until {closeHour}:00 come visit us.</p>
+            <button className="btn">Order</button>
+        </div>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
